2dca33bf1d34a81e2145731a00578af4f7623853
Nick Haglind <nhaglind@gmail.com>
2018-12-03 15:24:42 -0600

    day 2 part 1

---
+++ 2018/day_2/day_2.js
@@ -0,0 +1,43 @@
+/* dev tool snippet to extract data 
+var data = document.querySelector('pre').innerText.split('\n');
+console.log(data.join('", "'))
+*/
+
+let data = ["kqzxdenujwcsthbzgvyioflcsp", "vqwxdenujwcsthbmggyioflarp", "kqzxienujwcsthbmglyioclarp", "kuzxdetujwcsthbmgvyioflcrp", "kqnxdenujwcsthbmgvlooflarp", "kqzxdknpjwcsthwmgvyioflarp", "kgzxdenujwcsthbfgvyicflarp", "kqzxdenujrnsthbmgjyioflarp", "lqzxdeeujwcsthbmrvyioflarp", "iqfxdenujwcsthbmgvyiofyarp", "kvzxbenujwcstabmgvyioflarp", "kmzxdenujwcsthbmglyioolarp", "kqzxdenujhcsthbmgbyioflanp", "nqzxdenujwcsehbmgvsioflarp", "kqzlgenujwcsthbmgvyiofjarp", "kqzxdyfujwcsihbmgvyioflarp", "kqzxdsnujwcqthbmgvyiorlarp", "kqzxdenuywcsthbmgvyinflmrp", "knzxderujwcsthbmgvyioflaop", "kqxxdenujwczthbmgvyioflajp", "kqzxdevujwcsthbmgvyqoxlarp", "kqzxdenujwclmhbmgvyioslarp", "kqzldenujwcsthbmgvnisflarp", "kjtxdenujwcsthbmgvyfoflarp", "kqzxwenujwcstxbmgvyihflarp", "kqzxdenuhecsthbmgvyeoflarp", "kqzxdenhjwesthbmgvyioklarp", "kqkxdenujwcsthbcgvyiofgarp", "kqyxmenujwcsthbmgvyioflara", "kqzxdqnrjwcwthbmgvyioflarp", "kqzxdenufwcgyhbmgvyioflarp", "lqzxdenujwcsthbmtvyiofearp", "kqzxdenujwcsthbvgvthoflarp", "kqzxeenujwcsahbmgvyioflamp", "pqzxdenujwcsshbmjvyioflarp", "kqzxdesujwcstdbmgvyioflatp", "kqzxpenujwcsthimgvyioflhrp", "kqzxdmkujwcsthbmgvpioflarp", "kszxdenujwcsthbybvyioflarp", "kqzxdvnujwcsthbmgvyqoslarp", "kkzxdetujwcsthbmgvyiofltrp", "kqzxdenujwcsthomgvyiozlaro", "cqzfdenubwcsthbmgvyioflarp", "kqzxdenyjwcsthbmhvyiofldrp", "kqzxdenujwcsthbmghfiofxarp", "kmqxdebujwcsthbmgvyioflarp", "kqzxdenufwcsthbmvvypoflarp", "kqnxdenujwcsthbmgvtzoflarp", "bqzxdenujwcithbmgvyiohlarp", "kqzxdenurwrsthbmgvyioelarp", "kqzxdenujwcsthbmgpyiodlarl", "kqzxdengjwcxthbmgvjioflarp", "kizxdenujwcsnhqmgvyioflarp", "jqzxdenajwcsthbmnvyioflarp", "kqzcdenujwcsphbigvyioflarp", "kezxdenujwcsthbfgvyioflaqp", "kqzxdenujwcstybmgvyivfyarp", "kqzxdenujwcsthbmgvbiofsnrp", "kqzxdenujwcsthbmgvyhxfnarp", "kvzxdenqjfcsthbmgvyioflarp", "kqzxywnljwcsthbmgvyioflarp", "kqzxdenujwcsbhbzgvyioxlarp", "kqkxdenufwcsthbmgvyiofxarp", "dqzxddnujwcsthsmgvyioflarp", "yqrxdenujwcsthbagvyioflarp", "kqzxdenujwcsajbmgvyiovlarp", "kqztdunujwcsthbmgvyioilarp", "kqzxdequjwcsthbmgvyyoflarm", "kqzxdlnujwksthbmgvkioflarp", "tqvxdenujwcsthbmgveioflarp", "kqzndezupwcsthbmgvyioflarp", "kqzctsnujwcsthbmgvyioflarp", "kqzxdenujwmstkbmgvyioflgrp", "kqzxdenujwzsthdmgvyiofdarp", "kqzxdynujwcsthcmgvyioflasp", "kqzxdesujwcstybmgcyioflarp", "kqzxdenujwcsthbvgvyiyglarp", "kqzxpenujwcsthbogvyioflard", "khzxdenujwcsthbmgvyikflaep", "kqzxdedujwchthbmgvyeoflarp", "kxzxsepujwcsthbmgvyioflarp", "xqzxdenujwcsthbpgvyioelarp", "jfzxdenujwcsthbmgvyiollarp", "kqzxcenujwcethbmgvwioflarp", "kqzxdenujwcithbmgvysoflarg", "kqlxdenujwnsthbmgvyiotlarp", "wqzydenujwcsthbmgvyioftarp", "kqzxienuwwcsthbmgayioflarp", "kqzxdetujwcsthbmgvyhoflawp", "kqzxdqnujwrsthbmgvyxoflarp", "kqzxdenujwcvthbmgjiioflarp", "kqzxdenujwcjthbxgvaioflarp", "kqzxpenujwcsthymgvyioklarp", "kqzxdenujwcsthbmswygoflarp", "kqzxdenujwcsthbmgvyiaxiarp", "kqzxdenudkcsthbmgvyzoflarp", "kqzxdvndjwcsthbmgvyioflaxp", "kqzxdenujwcsthbmdvymoflvrp", "kqzxvenujwcsihbmgvyiofllrp", "kqzxdqnujwcsthbmgtyioflprp", "kqzxdenuuwcathbmgvsioflarp", "kqzrdenujwesthbjgvyioflarp", "kqzxdexujwcstzbmgvyyoflarp", "kqzxpenujwjstabmgvyioflarp", "kozxdenejwcsthbmgvpioflarp", "kbzxdenvjwcsthbmgvyiofsarp", "kolxdenujwcjthbmgvyioflarp", "kqzxdenujwcsthbmgvyiffsakp", "kqzxdelujwcsthbmlvyioflxrp", "kqzxdenugwcsthrmgvyioflprp", "kqzxdelujwcsthqmgvyiozlarp", "kqzxienujwosthbmgvykoflarp", "kqzxdeuujwicthbmgvyioflarp", "kqzxdenbjwcsthbmcvyaoflarp", "krzxdqnujwcsthbmgvyioflerp", "wqzxzenujwcsthbmgvyioclarp", "kqzxyenujwcsthbmgejioflarp", "kqzxdenujwcstsbmgvtidflarp", "kqnxdenejwcsthbmgvyioflara", "kqzsdmnujwcsthbmgvyioflaep", "kqzxdedujwnsthymgvyioflarp", "kqzxdenujwusthbmgnyioflarx", "kqzxlenujwcsthbmgayvoflarp", "kqzxdenujwcsthbmgvyiofngrh", "zqzxdenujwcsthbmgvyiofvxrp", "kqzydenujwmsthbmgvyiuflarp", "kqzxdenujkrsthbmdvyioflarp", "kqzxdlnujocsthbmgvyiofaarp", "kqzxdenujwcstybmgvyiofrwrd", "kqzxdenupwksthbmgvyiofbarp", "khzxdentjwcsthbmbvyioflarp", "kqzxdenujwcuphbmgvyihflarp", "kqzxdenhjwcgthbmgvyioflrrp", "kqzxdenujwcsthbmgvyiofakhp", "kqzxdenujwcstfkmgvyioflamp", "kqzxdenujqcsthbmgvkiorlarp", "kqzxdenujwcstvbmgvyioilasp", "kqzxdxnujwcsthbpgayioflarp", "kqzxdenupwysthbmgvyiofljrp", "kqzxdenujwcdthbmgvymoflarv", "kqnxdenujwcstvbmgvyixflarp", "kqjxdenujwcsthbmgvyikflurp", "kqsxdenulwcsthxmgvyioflarp", "bqzxbenujwcsahbmgvyioflarp", "vqzxdenujwcsthbmgvjzoflarp", "kqzhfenujwcsthimgvyioflarp", "eqzxdenujwcshhbmgnyioflarp", "kqzxdenujucstubmgvyicflarp", "kuzxdenuewcsthbmgvyiofuarp", "kqzxdenulwcsthbmgpyigflarp", "kqzxdebujwcsthbmgoyioflaro", "kqzxdenujwcuthbmgucioflarp", "kqzxdenujwcschpmgvyioflhrp", "kqzxfenujwcsthbmjvrioflarp", "kqzxdenujqcsthbmgvyndflarp", "kqzxdgnbjwcsthbmgvywoflarp", "kqzxdenujwcsthrmgtbioflarp", "yqzxdenyjwcsthbmgvyioflarg", "kqzxdenuxwxsthbmsvyioflarp", "kqzxdenujwcsthbugqyvoflarp", "qqzxdenujwcsahbmgoyioflarp", "kqsxdenudwcsthbmguyioflarp", "kqzxdenujwcstublgvyioflamp", "kqzxdemujwtstqbmgvyioflarp", "kqzxqvnajwcsthbmgvyioflarp", "kqzxoennjwcstbbmgvyioflarp", "kqzxfenujwcsthbmlvyioflwrp", "kqzjdunujwcsthhmgvyioflarp", "kqzxdenujwcqthbmgvyirfxarp", "kqzxdengjwcsthbmgvyiowlgrp", "kqgxdenujwcswhbmglyioflarp", "mqzxdekuuwcsthbmgvyioflarp", "kqzxdenujwdsthbmgbyiovlarp", "krzxdenlhwcsthbmgvyioflarp", "kqzxdenmjwcstqbmgvyioflanp", "kqzxdenujwcmthbmgvtioflyrp", "kqzxdenujwcsthbmgvaijflprp", "kqzxdenuywysqhbmgvyioflarp", "kqzxdenujwfsthbmgvyhoflark", "nqzcdefujwcsthbmgvyioflarp", "kqzxdenujrcsthgmgyyioflarp", "kqzxdqnujwzsthbmgvyioftarp", "kqzxdenujwcsthimgvyioolapp", "kqzxdenupwcsthbmggyioflyrp", "kqzxdjnujwcsthbvgvyioflarf", "kqzxdtnujwasthbmgvyiofuarp", "kqzxbensjzcsthbmgvyioflarp", "kqzxdenujwcsphbmwiyioflarp", "kqzgdenuowcsthbmgvyioflarh", "kmzxdenujwasthbmgvtioflarp", "kqzxdenujwcstybmgvyiofrard", "vqzxdenejwcsthbmglyioflarp", "kqhxdenujwcsmhbmgvyioflprp", "kqzxdnnujwcsthzsgvyioflarp", "kczxdenujwcsthbmgvyeoflaop", "kqzxdenujwcsxhbmgvaioflaap", "kqzxdenujwcsthbmgayiofnprp", "kqzxdvnujwcsthbmgvyipjlarp", "kqzxdenubwcskhbmgvyiofkarp", "kqzxdenujwcsthbgggyigflarp", "kqzxdenujncstabvgvyioflarp", "kqzxdenujwcstqimqvyioflarp", "kqzxeenujwcsdhbmgvyqoflarp", "kcpxdenujwcsthbmgvyioilarp", "kqwxuenujwcsthbmgvyiyflarp", "kqzxdwnujwcstgbmgvyioplarp", "kqzxdenuswcstvbmglyioflarp", "kqzxdenujwcsthabgvyiwflarp", "kqzxdpnujwcsthbmwvyiomlarp", "kqzxdenujwcdthbmgvcioffarp", "kqzxdenajwcsthbmtvyiofldrp", "kqzbnenujwcshhbmgvyioflarp", "kqzbdequiwcsthbmgvyioflarp", "kqzxdenuswcsohbmgzyioflarp", "kvzxdenujwcstdbmjvyioflarp", "kqzxoenujwcqthbmpvyioflarp", "kqzxhenujwcsthbmgoyiofoarp", "klzxdenujwczthbmgvyioflanp", "kqpxdenujwcsthbmgvyioflafz", "kqkxdenujwcstxbngvyioflarp", "kqzepenuxwcsthbmgvyioflarp", "bqzxdenujmcithbmgvyioflarp", "kdzxdjnujwcstnbmgvyioflarp", "kszxdenujwcsthbmgeyiofrarp", "kqzxdenijwcsthbmgvhiaflarp", "kqzadenujwcbtxbmgvyioflarp", "kqkxwenujwcsthbmgvyiowlarp", "pqzddenujwcsthbmgvyboflarp", "kqzxxenujwcsthbwgvyioflmrp", "kqzxdjnujwcsthbmgvyipilarp", "pqzxdenujwcsthbmgvyieflark", "sqzxdenujtcsthbmgiyioflarp", "kqzxdznujwcsthbmgvzioflajp", "kqzxdrnujqcsthbmgvyiofvarp", "gqzxdenujwcsthemgvlioflarp", "kqzxdenujjcsthbmgvuiofljrp", "kqzsdenujmcsthbmggyioflarp", "kqzxienujwcsthbmgvaioflaip", "kqzxdwnujwcstfkmgvyioflarp", "kqzqdenujwcithbmzvyioflarp", "kqzxdedpjwcsthbmgvyiofbarp", "kqzxdeaujwcbtdbmgvyioflarp", "kqzewenyjwcsthbmgvyioflarp", "kqzxddnujwcsthbmgyyiofrarp", "kqzxdtnujwcsthbmgvyiodlard", "kqzxdefujwcsthbmgvyiffwarp", "xczxdenujwcsthbmgvyooflarp", "kuzxdenujucsthbmgvykoflarp", "kqzxtenujwcwthbmgvyioplarp", "kqzxdencllcsthbmgvyioflarp"];
+let double = 0;
+let triple = 0;
+
+function count_letters(string) {
+  const x = {};
+  let char_double = 0;
+  let char_triple = 0;
+  [...string].forEach(element => {
+    if ((x[`${element}`]) === undefined) {
+        x[`${element}`] = 1;
+    } else {
+      x[element]++;
+    };
+    if (x[element] == 2) {
+      char_double++;
+    }
+    if (x[element] == 3) {
+      char_triple++;
+      char_double--;
+    }
+    if (x[element] == 4) {
+      char_triple--
+    }
+  });
+  if (char_double >= 1) {
+    double += 1;
+  }
+  if (char_triple >= 1) {
+    triple += 1; 
+  }
+}
+
+for (let i = 0; i < data.length; i++) {
+  count_letters(data[i]);
+}
+
+console.log(`checksum = ${double * triple}`);
